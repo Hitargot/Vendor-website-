af696503480db082ef8813033d3a94a6d69a6d90
Hitargot <Kolawoleabubakir6@gmail.com>
2024-07-21 18:35:14 +0800

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// firebaseConfig.js
+const firebaseConfig = {
+        apiKey: "AIzaSyAzsQ7kIfIGIeihqg5teN8mIz8hNlAk7mg",
+        authDomain: "exdollarium-3d474.firebaseapp.com",
+        databaseURL: "https://exdollarium-3d474-default-rtdb.firebaseio.com",
+        projectId: "exdollarium-3d474",
+        storageBucket: "exdollarium-3d474.appspot.com",
+        messagingSenderId: "748549990165",
+        appId: "1:748549990165:web:605ca9255078ea58f2eb1c",
+        measurementId: "G-T9QL24JR8M"
+  };
+  
+  export default firebaseConfig;
+  
